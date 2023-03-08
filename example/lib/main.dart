f5f5041e12a993d89979d315065ddab70125b3aa
Yevhenii Markosh <yevhenii.markosh@lanars.com>
2023-03-08 17:00:15 +0200

    Change content background

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -256,7 +256,7 @@ class _NavigationScreenState extends State<NavigationScreen>
   Widget build(BuildContext context) {
     final colors = Theme.of(context).extension<CustomColorsTheme>()!;
     return Container(
-      color: Colors.white,
+      color: Theme.of(context).colorScheme.background,
       child: ListView(
         children: [
           SizedBox(height: 64),
