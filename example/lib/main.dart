a2f1ba1cfd8e89c852596f2ac3ff117e466ddda7
Yevhenii Markosh <yevhenii.markosh@lanars.com>
2023-03-08 15:04:48 +0200

    Move HexColor to AppTheme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -276,15 +276,3 @@ class _NavigationScreenState extends State<NavigationScreen>
     );
   }
 }
-
-class HexColor extends Color {
-  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
-
-  static int _getColorFromHex(String hexColor) {
-    hexColor = hexColor.toUpperCase().replaceAll('#', '');
-    if (hexColor.length == 6) {
-      hexColor = 'FF' + hexColor;
-    }
-    return int.parse(hexColor, radix: 16);
-  }
-}
