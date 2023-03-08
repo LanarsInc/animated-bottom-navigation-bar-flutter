e1f700ad3614411a899c80833e611dd09ae8411b
Yevhenii Markosh <yevhenii.markosh@lanars.com>
2023-03-08 14:59:03 +0200

    Add app_theme

---
+++ example/lib/theme/app_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'custom_colors_theme.dart';
+
+class AppTheme {
+  static HexColor colorOrange = HexColor('#FFA400');
+  static HexColor colorGray = HexColor('#373A36');
+
+  static ThemeData get({required bool isLight}) {
+    final base = isLight ? ThemeData.light() : ThemeData.dark();
+    return base.copyWith(
+      useMaterial3: true,
+      extensions: [
+        CustomColorsTheme(
+          colorLabelColor: isLight ? Colors.grey : const Color(0xFF7A7FB0),
+          appBarBackgroundColor:
+          isLight ? Colors.blue : colorGray,
+          bottomNavigationBarBackgroundColor:
+          isLight ? Colors.blue : colorGray,
+        )
+      ],
+      appBarTheme: AppBarTheme(
+        backgroundColor: isLight ? Colors.blue : colorGray,
+      ),
+      colorScheme: base.colorScheme.copyWith(
+        surface: isLight ? Colors.blue : colorGray,
+        background: isLight ? Colors.blue : colorGray,
+      ),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
